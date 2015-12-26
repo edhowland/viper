000a51619f0e6edd4091f749cf01ca2ad20b2885
edhowland <ed.howland@gmail.com>
2015-12-26 21:45:24 +0000

    \'can insert and walk right,back, up and down in buffer in loop.rb\'

--- loop.rb
+++ loop.rb
@@ -9,12 +9,24 @@ require_relative 'lib/edit_buffer'
 require_relative 'inserter'
 require_relative 'bindings'
 
-loop do
-print 'E>> '
-  x = key_press
-  puts 'x is'
-  p x
-  puts 'map x is'
-  p map_key(x)
-  break if x == "\003" or x == 'q'
+@buffer = Buffer.new(File.read('./spec/def.rb'))
+@proc_bindings = make_bindings
+
+def perform key, buf=@buffer, bnd=@proc_bindings
+  prc = bnd[key]
+  raise RuntimeError.new("No mapping for #{key}") if prc.nil?
+  prc.call buf
 end
+  say 'editing: hit q when done'
+loop do
+  #x = key_press
+  #puts 'x is'
+  #p x
+  #puts 'map x is'
+  #p map_key(x)
+  key = map_key(key_press)
+  perform key
+  break if key == :key_q
+end
+
+puts @buffer.to_s
