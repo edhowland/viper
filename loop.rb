f0292723f8883d15f0c91b7c8493e0d792cba82f
edhowland <ed.howland@gmail.com>
2015-12-26 17:30:31 +0000

    \'refactored loop.rb to use key_press method to return raw console input like arrows\'

--- loop.rb
+++ loop.rb
@@ -2,19 +2,23 @@
 # loop.rb - event loop for editor
 require 'io/console'
 
-loop do
-x=''
-y=''
-  z=''
-print 'E>> '
-x = $stdin.getch
-if x == "\e"
-  y = $stdin.getch
-  if y == '['
-    z = $stdin.getch
+# read console input one keypress. If Escape sequence, keep consuming until done
+def key_press
+  x = y = z = ''
+  x = $stdin.getch
+  if x == "\e"
+    y = $stdin.getch
+    if y == "["
+      z = $stdin.getch
+    end
   end
+  x + y + z
 end
-puts 'x is '
-p x, y, z
+
+loop do
+print 'E>> '
+  x = key_press
+  puts 'x is'
+  p x
   break if x == "\003" or x == 'q'
 end
