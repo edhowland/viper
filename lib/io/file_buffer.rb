69bcb9a43bb5684d83108f2b36ede4ac59d2632e
edhowland <ed.howland@gmail.com>
2015-12-27 21:03:23 +0000

    \'created FileBuffer subclass to Buffer\'

---
+++ lib/io/file_buffer.rb
@@ -0,0 +1,12 @@
+# file_buffer.rb - class FileBuffer
+
+class FileBuffer < Buffer
+  def initialize filename
+    @fname = filename
+    if File.exist? @fname
+    super(File.read(@fname))
+    else
+    super ''
+  end
+  end
+end
