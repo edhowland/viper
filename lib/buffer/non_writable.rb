7a804d3c482180b2f36788581368afe9ef0e4273
edhowland <ed.howland@gmail.com>
2015-12-29 01:35:38 +0000

    \'added io/read_only_file_buffer.rb buffer/non_writable.rb\'

---
+++ lib/buffer/non_writable.rb
@@ -0,0 +1,12 @@
+# non_writable.rb - module NonWritable
+
+module NonWritable
+  def ins string
+    say BELL
+  end
+
+  def del
+    say BELL
+  end
+end
+
--- lib/ui/display_help.rb
+++ lib/ui/display_help.rb
@@ -5,6 +5,11 @@ Control Character Commands
 
 Ctrl-Q: Quit editing
 Ctrl-H: Display this Help Text
+Ctrl-S: Save the current buffer
+Ctrl-A: Move to the beginning of the line
+Ctrl-E: Move to the end of the line
+Ctrl-T: Move to the top of the buffer
+Ctrl-B: Move to the bottom of the buffer
 Ctrl-J: Display the character under or to the right of the cursor
 Ctrl-K: Display the current column
 Ctrl-L: Display the current line
