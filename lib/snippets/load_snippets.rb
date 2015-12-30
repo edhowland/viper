c3c3f103bd47c8d8ad42b8736b1403d981925265
edhowland <ed.howland@gmail.com>
2015-12-30 00:14:00 +0000

    \'changed load/save_snippets.rb to read from relative paths to config/sn__.json\'

--- lib/snippets/load_snippets.rb
+++ lib/snippets/load_snippets.rb
@@ -1,7 +1,7 @@
 # load_snippets.rb - method load_snippets
 
 def load_snippets
-  snippets_file = "/home/vagrant/src/viper/config/snippets.json" # FIXME
+  snippets_file = File.dirname(File.expand_path(__FILE__)) + '/../../config/snippets.json'
   content = File.read(snippets_file)
   $snippets = JSON.parse(content)
 end
--- lib/snippets/save_snippets.rb
+++ lib/snippets/save_snippets.rb
@@ -1,7 +1,7 @@
 # save_snippets.rb - method save_snippets
 
 def save_snippets
-  snippets_file = "/home/vagrant/src/viper/config/snippets.json" # FIXME
+  snippets_file = File.dirname(File.expand_path(__FILE__)) + '/../../config/snippets.json'
   content = JSON.generate($snippets)
   File.write(snippets_file, content)
 end
