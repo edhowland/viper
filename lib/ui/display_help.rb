9e5e83d1b574e085f59e880f4b236309655572f2
edhowland <ed.howland@gmail.com>
2016-01-02 19:14:39 +0000

    \'updated help text\'

--- lib/ui/display_help.rb
+++ lib/ui/display_help.rb
@@ -7,16 +7,23 @@ Ctrl-Q: Quit editing
 Ctrl-H: Display this Help Text
 Ctrl-Y: Speak the name of the current buffer
 Ctrl-S: Save the current buffer
-Ctrl-A: Move to the beginning of the line
-Ctrl-E: Move to the end of the line
-Ctrl-T: Move to the top of the buffer
-Ctrl-B: Move to the bottom of the buffer
+Shift+Home: Move to the beginning of the line
+Shift+End: Move to the end of the line
+Shift+PgUp: Move to the top of the buffer
+Shift+PgDn: Move to the bottom of the buffer
 Ctrl-J: Display the character under or to the right of the cursor
 Ctrl-K: Display the current column
 Ctrl-L: Display the current line
 Ctrl-O: Opens a new line below the current line
 Ctrl-P: Display up to the next 10 lines of the buffer
 
+Cut, Copy and Paste Commands
+-
+Shift+Right: Highlight and move right one character
+Shift+Left: Highlight and move left one character
+Ctrl-C: Copy highlighted text to clipboard
+Ctrl-X: Cut highlighted text to clipboard
+Ctrl-V: Paste clipboard text into current buffer at cursor
 Commands
 
 Ctrl-D: Enter editor debug mode (pry session). Ctrl-d again to return to editor session
