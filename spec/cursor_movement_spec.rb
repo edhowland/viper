d088406fb37f11ec5f94e5a6076703c674390c2d
edhowland <ed.howland@gmail.com>
2016-01-05 20:05:30 +0000

    \'added some checks to cursor_movement_spec.rb\'

--- spec/cursor_movement_spec.rb
+++ spec/cursor_movement_spec.rb
@@ -8,3 +8,28 @@ describe 'up on empty buffer' do
 
   specify { ->() { subject}.must_raise BufferExceeded; buf.col.must_equal 0}
 end
+
+
+describe 'simple single line' do
+  let(:buf) { Buffer.new 'line 1' }
+  subject {  buf.down }
+
+  specify {  ->{ subject }.must_raise BufferExceeded }
+
+end
+
+describe 'single line with newline' do
+  let(:buf) { Buffer.new "line 1\n" }
+  subject {  buf.down }
+
+  specify { subject; buf.at.must_equal nil }
+
+end
+
+describe '2 lines first line, empty line' do
+  let(:buf) { Buffer.new "line 1\n\n" }
+  subject {  buf.down; buf.at }
+
+  specify {  subject.must_equal "\n" }
+
+end
