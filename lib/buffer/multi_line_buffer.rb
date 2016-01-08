4f95db0202fd231de907ac0d452a5b9809061abc
edhowland <ed.howland@gmail.com>
2016-01-08 01:38:55 +0000

    \'added multi_line_buffer_spec.rb\'

---
+++ lib/buffer/multi_line_buffer.rb
@@ -0,0 +1,8 @@
+# mult_line_buffer.rb - class MultiLineBuffer
+
+class MultiLineBuffer
+  def initialize 
+    @a_buff = []
+    @b_buff = []
+  end
+end
---
+++ spec/multi_line_buffer_spec.rb
@@ -0,0 +1,3 @@
+# multi_line_buffer_spec.rb - specs for multi_line_buffer
+
+require_relative 'spec_helper'
