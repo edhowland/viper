a03949113ee96e4fdfa6fb986eec827fa0e60895
edhowland <ed.howland@gmail.com>
2016-01-08 01:56:21 +0000

    \'first run up on MultiLineBuffer\'

--- lib/buffer/multi_line_buffer.rb
+++ lib/buffer/multi_line_buffer.rb
@@ -2,7 +2,26 @@
 
 class MultiLineBuffer
   def initialize 
-    @a_buff = []
+    @a_buff = [Buffer.new('')]
     @b_buff = []
   end
+
+  def current
+    @a_buff[-1]
+  end
+
+  def line
+    current.line
+  end
+
+
+  def at
+    current.at
+  end
+
+  def ins string
+    current.ins string
+  end
+
+
 end
--- spec/multi_line_buffer_spec.rb
+++ spec/multi_line_buffer_spec.rb
@@ -1,3 +1,10 @@
 # multi_line_buffer_spec.rb - specs for multi_line_buffer
 
 require_relative 'spec_helper'
+describe 'insert char' do
+  let(:buf) {MultiLineBuffer.new }
+  subject { buf.ins 'a' }
+
+  specify { subject; buf.line.must_equal 'a' }
+
+end
