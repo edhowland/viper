ec9c81110b94085b8b7b08d23093d8aa0290ad2e
edhowland <ed.howland@gmail.com>
2016-01-22 22:10:23 +0000

    \'added lint pass 1 even indents back to lint method\'

--- lib/tools/lint.rb
+++ lib/tools/lint.rb
@@ -1,6 +1,19 @@
 # lint.rb - method lint - performs passes on buffer
 
 def lint buffer
-  say 'Lint OK'
+  @blackboard = []
+  @blackboard += lint_pass1(buffer)
+  @blackboard.unshift 'Lint pass 1 (Even indents) failed'  unless @blackboard.empty?
+
+  # Pass 2 - Check for consistant indenting/outdenting
+  # @blackboard.unshift lint_pass2(buffer)
+  # @blackboard.unshift 'Lint pass 2 (Consistant indent/outdents) failed' unless @blackboard.empty?
+
+  if @blackboard.empty?
+    say 'Lint OK'
+  else
+    say "lint failed\n"
+    @blackboard.each {|log| say "#{log}\n" }
+  end
 end
 
