634f9684786c3c6d266fbb94213d8ab3ce6296f6
edhowland <ed.howland@gmail.com>
2016-01-23 22:31:36 +0000

    \'added spec to key_press_spec.rb to check key_press before/after refactor . must be 83 successes\'

---
+++ spec/key_press_spec.rb
@@ -0,0 +1,14 @@
+# key_press_spec.rb - specs for key_press
+
+require_relative 'spec_helper'
+describe 'All possible key strings' do
+  @map = key_mappings
+
+  @map.each_pair do |k,v|
+    describe "key : #{k} value #{v}" do
+        before { $stdin = StringIO.new k }
+      subject { map_key(key_press) }
+      specify { subject.must_equal v }
+    end
+  end
+end
