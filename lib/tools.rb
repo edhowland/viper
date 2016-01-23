43ed17a967fc8c94bdb085b29067890952de9786
edhowland <ed.howland@gmail.com>
2016-01-23 01:54:41 +0000

    \'added lint_pass2 boilerplate\'

--- lib/tools.rb
+++ lib/tools.rb
@@ -2,6 +2,5 @@
 
 require_relative 'tools/lint_pass1'
 require_relative 'tools/compare_indent'
-
-
+require_relative 'tools/lint_pass2'
 require_relative 'tools/lint'
---
+++ lib/tools/lint_pass2.rb
@@ -0,0 +1,16 @@
+# lint_pass2.rb - method lint_pass2
+
+# Lint Pass 2 - compare indent levels between adjacent non-empty lines
+def lint_pass2 buffer
+  @blacklog = []
+
+  buffer.lines.each do |l|
+    unless l.empty?
+  # compare previous
+else
+  # skip
+end
+
+  end
+end
+
