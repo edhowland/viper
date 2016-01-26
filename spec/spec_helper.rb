0ce937afddff4a78606c161afac8e2e92f63e721
edhowland <ed.howland@gmail.com>
2016-01-26 22:00:51 +0000

    \'added filters to simplecov to skip spec/, now at 75% bad!\'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # spec_helper.rb - helper for Bungaku specs
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/config/'
+end
+
+
+#SimpleCov.start
 require_relative 'load_path'
 require 'minitest/autorun'
