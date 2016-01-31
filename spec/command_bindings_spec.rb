5287b913862ec86a19d27e5fa58904ee1409659c
edhowland <ed.howland@gmail.com>
2016-01-31 18:37:35 +0000

    \'chkpt command_bindings_spec.rb\'

---
+++ spec/command_bindings_spec.rb
@@ -0,0 +1,313 @@
+# command_bindings_spec.rb - specs for command_bindings
+
+require_relative 'spec_helper'
+
+describe 'nop' do
+  let(:buf) { ScratchBuffer.new }
+  let(:bind) { command_bindings }
+  let(:prc) { bind[:nop] }
+  subject { prc.call(buf) }
+
+  specify { subject }
+end
+
+
+describe '#{key}' do
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'q' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute buf,  "q" }
+
+  specify {subject.must_equal :quit }
+end
+
+
+describe 'q!' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'w' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'wq' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'rew!' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'r' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'r!' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 's' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'g' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'goto' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'n' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'p' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'o' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'k!' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'yank' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'help' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'check' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'pipe' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'pipe!' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'lint' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'new' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'report' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'slist' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'list' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'sedit' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'snip' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'apply' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'dump' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'load' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'assocx' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'assocf' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'assocd' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'tab' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'load_cov' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'cov' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'cov_report' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
+
+
+describe 'nop' do
+  let(:buf) { Buffer.new '' }
+  subject { parse_execute "" }
+
+  specify { skip 'not yet implemented' }
+end
