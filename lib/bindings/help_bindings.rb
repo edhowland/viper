e3191c4cd3ab2c169ee17a7780ac4897a8e69d53
edhowland <ed.howland@gmail.com>
2016-02-04 00:59:24 +0000

    \'added arrow keys help\'

--- lib/bindings/help_bindings.rb
+++ lib/bindings/help_bindings.rb
@@ -9,15 +9,29 @@ def punctuation_help
     :asterisk,:lparen, :rparen, :hyphen,
     :underline, :equals, :plus, :backslash, :pipe,
   :lbracket, :rbracket,:lbrace, :rbrace, 
-    :less, :greater, :question].each_with_object({}) {|k, h| h[k] = k.to_s }
+    :less, :greater, :question
+  ].each_with_object({}) {|k, h| h[k] = k.to_s }
 end
 
 def control_keys
   ('a'..'z').each_with_object({}) {|c, h| h["ctrl_#{c}".to_sym] = "control #{c}" }
 end
 
+def meta_help
+  [:meta_d, :meta_colon].each_with_object({}) {|c, h| h[c] = c.to_s }
+end
+
+def arrow_help
+  {
+    up: 'Moves up one line',
+    down: 'moves down one line',
+    left: 'moves one character to left',
+    right: 'moves one character to the right'
+  }
+end
+
 def help_bindings
   chars = ['a'..'z', 'A'..'Z', '0'..'9'].map {|r|identity_bindings(r) }.reduce({}) {|h, i| h.merge(i) } 
-  chars.merge(punctuation_help).merge(control_keys)
+  chars.merge(punctuation_help).merge(control_keys).merge(meta_help).merge(arrow_help)
 end
 
