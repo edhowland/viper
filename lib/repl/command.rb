4d72d540234ebe9caac8dcf0486be1c36bb246d3
edhowland <ed.howland@gmail.com>
2016-02-11 15:33:43 +0000

    \'chckpt expand_commands_spec.rb\'

--- lib/repl/command.rb
+++ lib/repl/command.rb
@@ -1,5 +1,16 @@
 # command.rb - method command - parses command, possibly expanding aliases
 
+def expand_commands commands
+  commands.split(/ *; */).map do |cmd|
+    cmd = expand_alias(cmd)
+    if cmd =~ /;/
+      expand_commands(cmd)
+    else
+      cmd
+    end
+  end.flatten
+end
+
 def command command_s, &blk
   yield command_s if block_given?
 end
