d644ba16371a0456133fb25d80294942feeefdcd
edhowland <ed.howland@gmail.com>
2016-02-13 01:40:53 +0000

    \'added star zero or more and plus one or more to parser.rb\'

--- spec/parser.rb
+++ spec/parser.rb
@@ -7,3 +7,26 @@ def match_thing buffer, regex, &blk
   buffer.fwd(result.length) if result
   !result.nil?
 end
+
+def match_word buffer, &blk
+  match_thing buffer, /^(\w+)/, &blk
+end
+
+def match_whitespace buffer, &blk
+  match_thing buffer, /^(\s+)/, &blk
+end
+
+def match_digit buffer, &blk
+  match_thing buffer, /^(\d)/
+end
+
+# recursion fns
+
+def star &blk
+  star(&blk) if yield
+  true
+end
+
+def plus &blk
+  yield && star(&blk)
+end
