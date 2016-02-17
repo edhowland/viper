f6e207a14d62948b84221d9034ac820012aa6162
edhowland <ed.howland@gmail.com>
2016-02-17 18:33:08 +0000

    \'working again command and find, rev_find with no pattern search pattern\'

--- lib/find_and_replace/find.rb
+++ lib/find_and_replace/find.rb
@@ -2,12 +2,21 @@
 
 def find buffer, pattern
   Viper::Session[:searches] ||= []
+  buffer.fwd if pattern.nil?
   buffer.srch_fwd (pattern || Viper::Session[:searches][-1] || //)
   Viper::Session[:searches] << pattern unless pattern.nil?
+  Viper::Session[:search_direction] = :find
 end
 
 def rev_find buffer, pattern
   Viper::Session[:searches] ||= []
   buffer.srch_back(pattern || Viper::Session[:searches][-1] || //) 
   Viper::Session[:searches] << pattern unless pattern.nil?
+  Viper::Session[:search_direction] = :rev_find
+end
+
+# search again in the last direction
+def again buffer
+  Viper::Session[:search_direction] ||= :find
+  send Viper::Session[:search_direction], buffer, nil
 end
