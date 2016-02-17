3d8aa051db85cab68213408aee9b77e5d2be65b8
edhowland <ed.howland@gmail.com>
2016-02-17 18:05:56 +0000

    \'can repeat last search in find, rev_find by not passing any argument\'

--- lib/find_and_replace/find.rb
+++ lib/find_and_replace/find.rb
@@ -1,10 +1,13 @@
 # find.rb - method find - match string pattern in buffer
 
 def find buffer, pattern
-  buffer.srch_fwd pattern
+  Viper::Session[:searches] ||= []
+  buffer.srch_fwd (pattern || Viper::Session[:searches][-1] || //)
+  Viper::Session[:searches] << pattern unless pattern.nil?
 end
 
-
 def rev_find buffer, pattern
-  buffer.srch_back pattern
+  Viper::Session[:searches] ||= []
+  buffer.srch_back(pattern || Viper::Session[:searches][-1] || //) 
+  Viper::Session[:searches] << pattern unless pattern.nil?
 end
