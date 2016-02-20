7628ca2923f51c10ce3f16413eee18cee1967fe8
edhowland <ed.howland@gmail.com>
2016-02-20 20:12:36 +0000

    \'made find and rev_find return boolean for found/not found patterns\'

--- lib/find_and_replace/find.rb
+++ lib/find_and_replace/find.rb
@@ -3,16 +3,20 @@
 def find buffer, pattern
   Viper::Session[:searches] ||= []
   buffer.fwd if pattern.nil?
+  memo = buffer.position
   buffer.srch_fwd (pattern || Viper::Session[:searches][-1] || //)
   Viper::Session[:searches] << pattern unless pattern.nil?
   Viper::Session[:search_direction] = :find
+  buffer.position != memo
 end
 
 def rev_find buffer, pattern
   Viper::Session[:searches] ||= []
+  memo = buffer.position
   buffer.srch_back(pattern || Viper::Session[:searches][-1] || //) 
   Viper::Session[:searches] << pattern unless pattern.nil?
   Viper::Session[:search_direction] = :rev_find
+  memo != buffer.position
 end
 
 # search again in the last direction
--- spec/find_spec.rb
+++ spec/find_spec.rb
@@ -15,3 +15,32 @@ describe 'rev_find' do
 
   specify { subject.must_equal 0 }
 end
+
+
+describe 'find returns false if not found' do
+  let(:buf) { Buffer.new 'hello world' }
+  subject { find(buf, 'good') }
+
+  specify { subject.must_equal false }
+end
+
+describe 'find returns true if found' do
+  let(:buf) { Buffer.new 'hello world' }
+  subject { find buf, 'world' }
+
+  specify { subject.must_equal true }
+end
+
+describe 'rev_find returns false if not found' do
+  let(:buf) { Buffer.new 'hello world' }
+  subject { buf.fin; rev_find buf, '0123' }
+
+  specify { subject.must_equal false }
+end
+
+describe 'rev_find returns true if fount' do
+  let(:buf) { Buffer.new 'hello world' }
+  subject { buf.fin; rev_find buf, 'ello' }
+
+  specify { subject.must_equal true }
+end
