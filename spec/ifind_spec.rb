b9026e87703ffd46e15f98d20c728c9d15224549
edhowland <ed.howland@gmail.com>
2016-02-24 20:04:08 +0000

    \'added some specs for ifind, irev_find\'

--- spec/ifind_spec.rb
+++ spec/ifind_spec.rb
@@ -1,3 +1,19 @@
 # ifind_spec.rb - specs for ifind
 
 require_relative 'spec_helper'
+
+describe 'ifind' do
+  let(:buf) { Buffer.new 'hello world' }
+  before { $stdin = StringIO.new "wor" + "\r" }
+  subject { ifind buf }
+
+  specify { subject.must_equal true; buf.position.must_equal 6 }
+end
+
+describe 'irev_find' do
+  let(:buf) { Buffer.new 'hello world' }
+  before { $stdin = StringIO.new 'llo' + "\r"; buf.fin }
+  subject { irev_find buf }
+
+  specify { subject.must_equal true; buf.position.must_equal 2 }
+end
