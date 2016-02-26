1efc6e5cac275dea0f3bd068da5a1e9a26d35e18
edhowland <ed.howland@gmail.com>
2016-02-26 13:11:59 +0000

    \'fixed spec fo config_path_spec.rb to infer correct location of config path\'

--- spec/config_path_spec.rb
+++ spec/config_path_spec.rb
@@ -2,8 +2,16 @@
 
 require_relative 'spec_helper'
 
+def my_path
+  File.dirname(File.expand_path(__FILE__))
+end
+
+def make_path dir
+  File.expand_path(my_path + '/' + dir) + '/'
+end
+
 describe 'config_path finds full path' do
   subject { config_path }
 
-  specify { subject.must_equal '/home/vagrant/src/viper/config/' }
+  specify { subject.must_equal make_path('../config') }
 end
