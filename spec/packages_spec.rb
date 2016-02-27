2280129df0db2daca2813bb86a40f5a588ffcd3b
edhowland <ed.howland@gmail.com>
2016-02-27 23:46:58 +0000

    \'removed magic path literal from packages_spec.rb\'

--- spec/packages_spec.rb
+++ spec/packages_spec.rb
@@ -2,6 +2,11 @@
 
 require_relative 'spec_helper'
 
+def home_path path
+  File.expand_path("~").pathmap("%p/#{path}")
+end
+
+
 describe 'package init resolves name' do
   let(:pkg) { Viper::Package.new 'debug' }
   subject { pkg.name }
@@ -13,11 +18,11 @@ describe 'package init resolves full path from Viper::Packages::PATH_NAME' do
   let(:pkg) { Viper::Package.new 'debug' }
   subject { pkg.path }
 
-  specify { subject.must_equal '/home/vagrant/.viper/packages/debug/' }
+  specify { subject.must_equal home_path('.viper/packages/debug/') }
 end
 
 describe 'Viper::Packages::PACKAGE_PATH includes our home base' do
   subject { Viper::Packages::PACKAGE_PATH }
 
-  specify { subject.must_equal ['/home/vagrant/.viper/packages/'] }
+  specify { subject.must_equal [home_path('.viper/packages/')] }
 end
