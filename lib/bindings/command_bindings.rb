fe059a46627e2dfea7458baab03642562a243c56
edhowland <ed.howland@gmail.com>
2016-02-29 21:41:04 +0000

    \'started move of simplecov support to viper_simplecov package\'

--- lib/bindings/command_bindings.rb
+++ lib/bindings/command_bindings.rb
@@ -103,7 +103,7 @@ def command_bindings
     tab: ->(b, *_args) { handle_tab(b) },
 
     # Code Coverage support from simplecov
-    load_cov: ->(_b, *args) { load_cov args[0]; say "Coverage repor #{args[0]} loaded" },
+#    load_cov: ->(_b, *args) { load_cov args[0]; say "Coverage repor #{args[0]} loaded" },
     cov: ->(b, *_args) { sc = ScratchBuffer.new; sc.name = "Coverage report for #{b.name}"; cov(sc, b.name); $buffer_ring.unshift sc; sc.beg; say sc.name; say sc.line },
     cov_report: ->(_b, *_args) { cov_report; say $buffer_ring[0].name.to_s },
 
