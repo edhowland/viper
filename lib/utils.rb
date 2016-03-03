dc07913b4009caaf9c2a7a4a8b7e4c410138dbfa
edhowland <ed.howland@gmail.com>
2016-03-03 17:23:31 +0000

    \'removed path_shortner stuff, migrated to viper_simplecov package\'

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,4 +1,4 @@
 # utils.rb - requires for utils/
 
-require_relative 'utils/path_shortener'
+#require_relative 'utils/path_shortener'
 require_relative 'utils/config_path'
--- spec/path_shortener_spec.rb
+++
@@ -1,9 +0,0 @@
-# path_shortener_spec.rb - specs for path_shortener
-
-require_relative 'spec_helper'
-
-describe 'path_shortener full' do
-  subject { path_shortener(File.expand_path(__FILE__)) }
-
-  specify { subject.must_equal '.../src/viper/spec/path_shortener_spec.rb' }
-end
