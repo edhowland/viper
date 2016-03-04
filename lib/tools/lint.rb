68011dabee46815a9119a4f4b572e38717120e78
edhowland <ed.howland@gmail.com>
2016-03-04 17:59:52 +0000

    \'final refactor for lint method\'

--- lib/tools/lint.rb
+++ lib/tools/lint.rb
@@ -1,6 +1,18 @@
 # lint.rb - method lint - performs passes on buffer
 
+# TODO: class documentation
+class LintCheckerNotFoundForAssociation < RuntimeError
+  def initialize lang 
+    super "No lint checker found for association #{lang}"
+  end
+end
+
 def lint(buffer)
-  
-  send "check_ruby_lint".to_sym, buffer
+  lang = buffer.association.to_s
+  lint_method ="check_#{lang}_lint".to_sym 
+  begin
+    send lint_method, buffer
+  rescue => err
+    raise LintCheckerNotFoundForAssociation.new lang
+  end
 end
