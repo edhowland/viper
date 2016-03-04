43b1e6fefeaa9aba147b10f895e1fb3e00a9d95b
edhowland <ed.howland@gmail.com>
2016-03-04 22:41:46 +0000

    \'further refactor of tools.rb to remove commented requires\'

--- lib/tools.rb
+++ lib/tools.rb
@@ -1,9 +1,3 @@
 # tools.rb - requires for tools/
 
-#require_relative 'tools/lint_pass1'
-#require_relative 'tools/compare_indent'
-#require_relative 'tools/lint_pass2'
-#require_relative 'tools/lint_pass3'
-#require_relative 'tools/check_ruby_lint'
-
 require_relative 'tools/lint'
