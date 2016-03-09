e248039b6c6cb52cd283a25d9e76d9d2ce8579c6
edhowland <ed.howland@gmail.com>
2016-03-09 17:49:37 +0000

    \'added check_viper_syntax so boilerplate for actually checking .viperc and .viper files\'

--- lib/system.rb
+++ lib/system.rb
@@ -1,4 +1,6 @@
 # system.rb - requires for system/
 
 require_relative 'system/shell'
+require_relative 'system/check_viper_syntax'
+
 require_relative 'system/check_lang_syntax'
---
+++ lib/system/check_viper_syntax.rb
@@ -0,0 +1,5 @@
+# check_viper_syntax.rb - method check_viper_syntax.rb
+def check_viper_syntax buffer
+  say 'Syntax OK'
+  true
+end
