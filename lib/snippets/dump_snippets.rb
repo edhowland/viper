c9eb358c8b44af96e250a2e4c78d674a10bab143
edhowland <ed.howland@gmail.com>
2016-03-12 20:38:08 +0000

    \'updated dump_snippets to prefer the tilde path .viper/snippets if no other existing file can be found\'

--- lib/snippets/dump_snippets.rb
+++ lib/snippets/dump_snippets.rb
@@ -1,7 +1,7 @@
 # dump_snippets.rb - method dump_snippets
 
 def dump_snippets(cascade, path)
-  snippets_file = Viper::Snippets::Searcher.locate(path, :ignore_missing => true)
+  snippets_file = Viper::Snippets::Searcher.locate(path, :ignore_missing => true) || Viper::Snippets::Searcher.home_path(path)
 
   snippets = $snippet_cascades[cascade]
   raise SnippetCollectionNotFound if snippets.nil?
