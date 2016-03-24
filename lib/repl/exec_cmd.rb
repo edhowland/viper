ec48f4603fea83c088d6ff554c81925ff6654e83
edhowland <ed.howland@gmail.com>
2016-03-24 15:26:13 +0000

    \'added late binding of command variables to exec_cmd\'

--- lib/repl/exec_cmd.rb
+++ lib/repl/exec_cmd.rb
@@ -1,6 +1,24 @@
 # exec_cmd.rb - method exec_cmd
 
+def symbol_if_colon string
+  if string =~ /^:(.+)$/
+    $1.to_sym
+  else
+    string
+  end
+end
+
+def deref_variable string
+  result = symbol_if_colon(string)
+  if result.instance_of?(Symbol)
+    Viper::Variables[result]
+  else
+    result
+  end
+end
+
 def exec_cmd(command, buffer, *args)
+  args = args.map { |e| deref_variable(e) }
   command_p = Viper::Session[:commands][command]
   raise CommandNotFound.new(command) if command_p.nil?
   command_p.call(buffer, *args)
