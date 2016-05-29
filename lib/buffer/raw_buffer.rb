e181664e7be07bcf081f799e1b7fcf566f6e4738
edhowland <ed.howland@gmail.com>
2016-05-29 21:26:33 +0000

    \'got at to work with backslash r, t and b for RawBuffer\'

--- lib/buffer/raw_buffer.rb
+++ lib/buffer/raw_buffer.rb
@@ -1,9 +1,19 @@
 # raw_buffer.rb - class RawBuffer
 
- class RawBuffer < Buffer
-   def at
-    super
-   end
-
- end
+# Represents a normal buffer, but translates raw characters to audible names
+class RawBuffer < Buffer
+  def at
+    value = super
+    case value
+    when "\r"
+      "return"
+    when "\t"
+      "tab"
+    when "\b"
+      "back tab"
+    else
+      value
+    end
+  end
+end
  
--- spec/raw_buffer_spec.rb
+++ spec/raw_buffer_spec.rb
@@ -4,7 +4,21 @@ require_relative 'spec_helper'
 
 describe RawBuffer do
   let(:buf) { RawBuffer.new 'if ' }
-  subject { buf.line }
+  subject { buf }
   
-  specify { subject.must_equal 'if ' }
+  specify { subject.line.must_equal 'if ' }
+
+  it 'should leave char unchanged' do
+    subject.at.must_equal 'i'
+  end
+
+  describe 'when seeing a return' do
+    let(:buf) { RawBuffer.new "\r" }
+    
+    it 'should say return' do
+      buf.at.must_equal 'return'
+    end
+  end
+  
 end
+
