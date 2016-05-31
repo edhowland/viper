4b0cb8826b47de070de56655862183a08dbd729e
edhowland <ed.howland@gmail.com>
2016-05-31 20:10:32 +0000

    \'first attempt at making simpler vish command processor, leaving out the impliicit buffer\'

--- lib/repl/vish.rb
+++ lib/repl/vish.rb
@@ -1,8 +1,23 @@
 # vish.rb - methods vish, vish! and vish_file
 
+# vish! parses and executes a single command
+def vish! string
+  sexps = parse!(string)
+  sexps = dealias_sexps(sexps)
+  fail CommandNotVerified.new unless command_verified?(sexps)
+  result = nil
+  sexps.each do |s|
+    cmd, args = s
+    result = exec_cmd cmd, *args
+  end
+
+  result
+end
+
 def vish_file path
   tmp_buff = ScratchBuffer.new
   load_rc path do |l|
-      perform!(l) { tmp_buff }
+    perform!(l) { tmp_buff }
   end
 end
+
