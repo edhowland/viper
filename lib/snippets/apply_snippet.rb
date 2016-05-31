ec22afea74f505465ff6d1ecc604dc6e503c15fb
edhowland <ed.howland@gmail.com>
2016-05-31 00:15:55 +0000

    \'stop here. suppress audio when applying snippet\'

--- lib/snippets/apply_snippet.rb
+++ lib/snippets/apply_snippet.rb
@@ -1,6 +1,6 @@
 # apply_snippet.rb - method apply_snippet
 
- # applies the snippet from the collection to the buffer
+# applies the snippet from the collection to the buffer
 def apply_snippet(cascade, name, buffer)
   snippets = $snippet_cascades[cascade]
   raise SnippetCollectionNotFound if snippets.nil?
@@ -9,11 +9,13 @@ def apply_snippet(cascade, name, buffer)
 
   bindings = make_bindings
   ksyms = convert_snip_to_keys(snip)
-  buffer.remember do |b|
-    #b.ins snip
-    ksyms.each do |k|
-      prc = bindings[k]
-      prc.call(b)
+  suppress_audio do
+    buffer.remember do |b|
+
+      ksyms.each do |k|
+        prc = bindings[k]
+        prc.call(b)
+      end
     end
   end
 
