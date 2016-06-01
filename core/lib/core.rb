bfdc99620a874f50cff0ff05591e2e8c992efa89
edhowland <ed.howland@gmail.com>
2016-06-01 18:27:02 +0000

    \'modified vish! to use simpler procs in command search path after parsing\'

--- core/lib/core.rb
+++ core/lib/core.rb
@@ -1,6 +1,6 @@
 # core.rb - objjects, requires for viper/core package
 commands = {
-  say: ->(*args) { print args }
+  say: ->(*args) { print args.join(' ') }
  }
 
 Viper::Commands::CMD_PATH.unshift commands
--- lib/repl/vish.rb
+++ lib/repl/vish.rb
@@ -1,25 +1,37 @@
 # vish.rb - methods vish, vish! and vish_file
 
+# namespace for Viper stuff
 module Viper
+  # Command stuff
   module Commands
-    CMD_PATH = []
+    # commandsearch path
+    # preloaded with minimum commands
+    CMD_PATH = [
+      exit: ->(*args) { exit },
+      package: ->(*args) { package_load args[0] },
+      require: ->(*args) { require args[0] },
+      set: ->(*args) { Viper::Variables.set(args[0], args[1]) }
+    ]
   end
 end
 
+# returns non-nil if command exists in CMD_PATH search space
 def resolve_cmd cmd
-  Viper::Commands::CMD_PATH.reduce(nil) { |e, i| i || e[cmd] }
+  Viper::Commands::CMD_PATH.reduce(nil) { |i, e| i || e[cmd] }
 end
 
 # vish! parses and executes a single command
 def vish! string
   sexps = parse!(string)
   sexps = dealias_sexps(sexps)
-  fail CommandNotVerified.new unless command_verified?(sexps)
+  #fail CommandNotVerified.new unless command_verified?(sexps)
   result = nil
   sexps.each do |s|
     cmd, args = s
-    fail "command #{cmd} not found" if resolve_cmd(cmd).nil?
-    result = exec_cmd cmd, *args
+    cmd_proc = resolve_cmd cmd
+    fail "command #{cmd} not found" if cmd_proc.nil?
+    cmd_proc.call *args
+    # result = exec_cmd cmd, *args
   end
 
   result
@@ -28,7 +40,8 @@ end
 def vish_file path
   tmp_buff = ScratchBuffer.new
   load_rc path do |l|
-    perform!(l) { tmp_buff }
+    # perform!(l) { tmp_buff }
+    vish! l
   end
 end
 
