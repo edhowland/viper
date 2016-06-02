3ec8e74e4c8d121acc9880ec79fed68cc54be19d
edhowland <ed.howland@gmail.com>
2016-06-02 22:07:48 +0000

    \'added simple cat command\'

--- core/lib/core.rb
+++ core/lib/core.rb
@@ -1,7 +1,8 @@
 # core.rb - objjects, requires for viper/core package
 commands = {
   say: ->(*args, env:{}) { print args.join(' ') },
-  echo: ->(*args, env:{}) { env[:out].write(args.join(' ')) }
+  echo: ->(*args, env:{}) { env[:out].write(args.join(' ')) },
+  cat: ->(*args, env:{}) { env[:out].write(env[:in].read) }
  }
 
 Viper::Commands::CMD_PATH.unshift commands
