c8c5dcc5e232ea630f758b36d891efe976c5d513
edhowland <ed.howland@gmail.com>
2016-06-05 01:17:49 +0000

    \'added view component called speech here istead of view in viper2 event loop\'

--- lib/vfs/mode.rb
+++ lib/vfs/mode.rb
@@ -3,11 +3,29 @@
 Viper::VFS["mode"] = {
   "vedit" => {
     "bind" => {
-      "key_i" => "echo i >+ /buf/0",
-      "key_j" => "echo j >+ /buf/0",
       "ctrl_q" => "exit",
       "ctrl_d" => "pry",
       "meta_colon" => "vish"
     }
     }
   }
+
+# add letters to /mode/vedit/bind
+(('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a).each do |letter|
+  Viper::VFS["mode"]["vedit"]["bind"]["key_#{letter}"] = "echo #{letter} >+ /buf/0"
+end
+
+
+def add_special_letter letter, value
+    Viper::VFS["mode"]["vedit"]["bind"]["#{letter}"] = "echo #{value} >+ /buf/0"
+  end
+
+  add_special_letter :space, ' '
+  
+
+# setup /mode/vedit/speech/*
+Viper::VFS["mode"]["vedit"]["speech"] = { "space" => "say space" }
+
+(('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a).each do |letter|
+  Viper::VFS["mode"]["vedit"]["speech"]["key_#{letter}"] = "say #{letter}"
+end
