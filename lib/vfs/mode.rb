60445eca566c02a2bdf098303f4a1067b71f3c5d
edhowland <ed.howland@gmail.com>
2016-06-05 00:02:09 +0000

    \'added modes like /mode/vedit/bind with keys i,j\'

---
+++ lib/vfs/mode.rb
@@ -0,0 +1,10 @@
+# mode - buffers /mode/vedit
+
+Viper::VFS["mode"] = {
+  "vedit" => {
+    "bind" => {
+      "key_i" => "echo i >+ /buf/0",
+      "key_j" => "echo j >+ /buf/0"
+    }
+    }
+  }
