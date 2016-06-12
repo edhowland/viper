7e7a753dd9f2c1979bdbc012530e8af18c3691a1
edhowland <ed.howland@gmail.com>
2016-06-12 20:37:49 +0000

    \'added pass-thru specs for ArgumentResolver in argument_resolver_spec.rb\'

---
+++ spec/y/argument_resolver_spec.rb
@@ -0,0 +1,46 @@
+# argument_resolver_spec.rb - specs for argument_resolver
+
+require_relative 'spec_helper'
+
+describe ArgumentResolver do
+  describe ':redirect_to file' do
+    let(:env) { {in: nil, out: nil, err: nil, frames: [{}] } }
+    let(:res) { ArgumentResolver.new env }
+    subject { res.resolve [:redirect_to, 'file'] }
+    it 'should be nil' do
+      subject.must_equal nil
+    end
+    it 'should set env[:out] to a File object' do
+      subject
+      env[:out].must_be_instance_of File
+    end
+    after { env[:out].close if env[:out].instance_of?(File) }
+  end
+  describe 'redirect_from file' do
+    let(:env) { {in: nil, out: nil, err: nil, frames: [{}] } }
+    let(:res) { ArgumentResolver.new env }    
+    subject { res.resolve [:redirect_from, 'file'] }
+    it 'should be nil' do
+      subject.must_equal nil
+    end
+    it 'should set :in to File object' do
+      subject
+      env[:in].must_be_instance_of File
+    end
+    after { env[:in].close if env[:in].instance_of? File }
+
+  end
+  describe ':append_to file' do
+    let(:env) { {in: nil, out: nil, err: nil, frames: [{}] } }
+    let(:res) { ArgumentResolver.new env }    
+    subject { res.resolve [:append_to, 'file'] }
+    it 'should be nil' do
+      subject.must_equal nil
+    end
+    it 'should be instance of file' do
+      subject
+      env[:out].must_be_instance_of File
+    end
+    after { env[:out].close if env[:out].instance_of?(File) }
+  end
+end
