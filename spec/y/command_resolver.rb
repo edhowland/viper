c41319d0b9e157d9559b4ea5f71d80df39ccea24
edhowland <ed.howland@gmail.com>
2016-06-15 18:52:13 +0000

    \'added Function storage to CommandResolver, functions shadow builtins\'

--- spec/y/command_resolver.rb
+++ spec/y/command_resolver.rb
@@ -1,17 +1,23 @@
-# command_resolver - classCommandResolver
+# command_resolver - classCommandResolver - returns obj that responds to call
 
 require_relative 'echo'
 require_relative 'cat'
 require_relative 'bad'
 
 class CommandResolver
+  @@storage = {}
   class << self
   def [] cmd
+    return @@storage[cmd] if @@storage.has_key? cmd
     return Echo.new if cmd == :echo
     return Cat.new if cmd == :cat
     return Bad.new if cmd == :bad
     Command.new cmd
   end
+  def []=(key, value)
+    @@storage[key] = value
+  end
+  
   end
 end
 
