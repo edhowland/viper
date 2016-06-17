f083f177767f629dffcff1e34a0ecd6a5936b9f8
edhowland <ed.howland@gmail.com>
2016-06-17 23:45:49 +0000

    \'seem to be properly handling alias recursive calls\'

--- spec/y/alias.rb
+++ spec/y/alias.rb
@@ -1,17 +1,25 @@
 # alias - class Alias - callable string that replaces value of name
 
 class Alias
+  @@calls = []
   def initialize name, expansion
     @name = name
     @expansion = expansion
   end
   attr_reader :name, :expansion
   def call *args, env:
+    if @@calls.member? @name
+      fail "recursive call to this alias: #{@name}"
+    else
+      @@calls << @name
+    end
     arg = args.join(' ')
     statement = @expansion + ' ' + arg
     sexps = Visher.parse! statement
     exc = Executor.new env
-    exc.execute! sexps
+    result = exc.execute! sexps
+    @@calls.pop
+    result
   end
 end
 
