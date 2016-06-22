f7e67e2b16394b905cd3554cf20f553d35dee468
edhowland <ed.howland@gmail.com>
2016-06-22 23:45:14 +0000

    \'added push, pop_and_store to frame_stack.rb , for creating Assignment frame and pushing it when NullCommand is called\'

--- spec/v2/bin/null_command.rb
+++ spec/v2/bin/null_command.rb
@@ -1,1 +1,8 @@
-# null_command - class 
+# null_command - class NullCommand - for when no command in Statement
+
+
+class NullCommand
+  def call frames:
+    frames.pop_and_store
+  end
+end
--- spec/v2/frame_stack.rb
+++ spec/v2/frame_stack.rb
@@ -14,5 +14,15 @@ class FrameStack
   def []= key, value
     @frames[-1][key] = value
   end
+  # push a new frame onto the stack. Possibly for assignment
+  def push
+    @frames.push({})
+  end
+  # pop the last frame (presumably the assignment frame) and merge it back to the
+  # previous frame
+  def pop_and_store
+    locals = @frames.pop
+    @frames[-1].merge! locals
+  end
 end
 
