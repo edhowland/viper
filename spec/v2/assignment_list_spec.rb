a52e233addde438e50c3fb7bcdb483d48dcee69a
edhowland <ed.howland@gmail.com>
2016-06-22 01:02:04 +0000

    \'corrected assignment_list_spec.rb\'

--- spec/v2/assignment_list_spec.rb
+++ spec/v2/assignment_list_spec.rb
@@ -7,11 +7,10 @@ describe AssignmentList do
   let(:fs) { FrameStack.new }
   let(:a1) { Assignment.new :a1, StringLiteral.new('a1') }
   let(:b2) { Assignment.new :b2, StringLiteral.new('b2') }
-  let(:c3) { Assignment.new :c2, Deref.new(:a1) }
-  let(:alist) { AssignmentList.new [a1, b2, c3 ] }
+  let(:c3) { Assignment.new :c3, Deref.new(:a1) }
+  let(:alist) { AssignmentList.new [ a1, b2, c3 ] }
   before { alist.call frames:fs }
   subject { fs[:c3] }
-  
   it 'should be a1' do
     subject.must_equal 'a1'
   end
