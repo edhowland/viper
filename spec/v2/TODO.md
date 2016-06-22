0799069b95565732f5e5737656b3ac7b5125f374
edhowland <ed.howland@gmail.com>
2016-06-22 15:47:50 +0000

    \'added string interpolation to StringLiterals and updated TODO.md\'

--- spec/v2/TODO.md
+++ spec/v2/TODO.md
@@ -1,6 +1,7 @@
 # TODO
 
+0. Implement specs for arg list
 1. implement redirect_std* 
 2. (opt) Use Fibers. resume from within open {|f| block...
-3.
+3. add tests to StringLiteral, test in assignment*_spec USING string interpolation
 4. Expand Argument to return a possible ArgumentList if given a name with a glob E.g. file*.rb, etc.
--- spec/v2/string_literal.rb
+++ spec/v2/string_literal.rb
@@ -4,8 +4,24 @@
 class StringLiteral < QuotedString
   def call frames:
     string = super
-    # interpolate ...
-    string
+    interpolate string, frames:frames
+  end
+  protected
+
+    def decurlify arg
+    m = arg.match /:{([_\w]+)\}/
+    return "" if m.nil?
+    m.captures.first
+  end
+  def interpolate string, frames:
+    string.gsub(/:\{[_\w]+\}/).each do |v|
+      var_s = decurlify(v)
+      if var_s.nil?
+        ''
+      else
+        frames[var_s.to_sym]
+      end
+    end
   end
 end
 
