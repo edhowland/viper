454f92177e3eb19d5fd516d16fffb7827bd41300
edhowland <ed.howland@gmail.com>
2016-06-28 20:40:35 +0000

    \'removed surplus ruby files\'

--- spec/v2/element.rb
+++
@@ -1,8 +0,0 @@
-# element - module Element - sortable - one of Redirection, Glob or Assignment
-
-module Element
-  include Comparable
-  def <=> other
-    self.ordinal <=> other.ordinal
-  end
-end
--- spec/v2/snc.rb
+++
@@ -1,3 +0,0 @@
-
-ruby -e 'puts ARGV[0].split("_").map {|e| e.capitalize }.join("")' snake_case
-                                                                                     
