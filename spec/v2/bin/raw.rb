cb9bcdd18f3d36d903b32ea4d2b6b7dd977adf24
edhowland <ed.howland@gmail.com>
2016-07-24 20:21:45 +0000

    \'added remedy gem, used in bin/raw.rb\'

--- spec/v2/bin/raw.rb
+++ spec/v2/bin/raw.rb
@@ -1,12 +1,18 @@
 # raw - class Raw,  - command raw - reads one char from $stdin into var
 
 
+require 'remedy'
+
 class Raw
+  include Remedy
+  def getch
+    Keyboard.get.seq
+  end
   def call *args, env:, frames:
     if args[0] == '-'
-      env[:out].write $stdin.getch
+      env[:out].write getch
     else
-      frames[args[0].to_sym] = $stdin.getch
+      frames[args[0].to_sym] = getch
       frames.merge
     end
     true
