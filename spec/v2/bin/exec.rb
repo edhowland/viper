8e7cdd9ec0855c5ccae48e79b7fb292281f49e74
edhowland <ed.howland@gmail.com>
2016-07-28 18:54:21 +0000

    \'added exec command, needed for complex functions\'

---
+++ spec/v2/bin/exec.rb
@@ -0,0 +1,13 @@
+# exec - class Exec - command exec { block } - runs block of first arg
+
+
+class Exec
+  def call *args, env:, frames:
+    block = args.shift
+    if block.nil? || !block.instance_of?(Block)
+      env[:err].puts 'exec: block must be first arg'
+      return false
+    end
+    block.call env:env, frames:frames
+  end
+end
