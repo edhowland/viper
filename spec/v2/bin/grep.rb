d3612bd0fa4c64562916ec32b14f009d9faa2dfa
edhowland <ed.howland@gmail.com>
2016-07-28 18:47:04 +0000

    \'simplified grep to act more naturally with its linux cousin\'

--- spec/v2/bin/grep.rb
+++ spec/v2/bin/grep.rb
@@ -2,26 +2,16 @@
 
 
 class Grep
-  def regex_line node, &blk
-    line = node['line']
-    yield line.string
-  end
   def call *args, env:, frames:
-  root = frames[:vroot]
-  node = root.wd
-  result = nil
-  until node.nil?
-    result = regex_line(node) {|s| s.index /2/ }
-    break unless result.nil?
-    node = node['nl']
-  end
-  unless result.nil?
-    line = node['line']
-    env[:out].puts line.string 
-    root.wd = node
-      true
-    else
-      false
-  end
+    pattern = args.shift
+    regex = %r{#{pattern}}
+    result = false
+    env[:in].read.each_line do |l|
+      if l =~ regex
+        result = true
+        env[:out].puts l.chomp
+      end
+    end
+    result
   end
 end
