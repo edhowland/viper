eacf1d1ea853484aa991db403ab36fe6effce396
edhowland <ed.howland@gmail.com>
2016-07-28 20:07:36 +0000

    \'added true, false commands for completeness\'

---
+++ spec/v2/bin/false.rb
@@ -0,0 +1,8 @@
+# false - class False  - command false - returns false
+
+
+class False
+  def call *args, env:, frames:
+    false
+  end
+end
---
+++ spec/v2/bin/true.rb
@@ -0,0 +1,8 @@
+# true - class True - command true - returns true
+
+
+class True
+  def call *args, env:, frames:
+    true
+  end
+end
