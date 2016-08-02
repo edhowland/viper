816a52e62ca95bfc633daf78bf91b0a082b62ac3
edhowland <ed.howland@gmail.com>
2016-08-02 14:12:10 +0000

    \'corrected cd behaviour on sub_shell.rb , restores previous environment\'

--- spec/v2/sub_shell.rb
+++ spec/v2/sub_shell.rb
@@ -3,13 +3,28 @@
 class SubShell
   def initialize block
     @block = block
+    @pwd = ''
+    @oldpwd = ''
+    @vm = nil
+  end
+  def save_pwd vm
+    @pwd = vm.fs[:pwd]
+    @oldpwd = vm.fs[:oldpwd]
+  end
+  def restore_pwd vm
+    vm.cd @pwd, env:vm.ios, frames:vm.fs
+    vm.fs[:oldpwd] = @oldpwd
   end
   def call env:, frames:
-    vm = frames.vm._clone
+    @vm = frames.vm
+    save_pwd @vm
+    vm = @vm._clone
     begin
       vm.call @block
     rescue VirtualMachine::BreakCalled => err
       return true
+    ensure
+      restore_pwd @vm
     end
   end
   def ordinal
