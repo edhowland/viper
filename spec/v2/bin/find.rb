fc5771cffd3d67514df956552bcdd760c44e5176
edhowland <ed.howland@gmail.com>
2016-08-03 00:58:32 +0000

    \'temporary hold on find command, almost usually works, try for search function\'

--- spec/v2/bin/find.rb
+++ spec/v2/bin/find.rb
@@ -20,9 +20,21 @@ class Find
   end
   def call *args, env:, frames:
     @source, @filter, @exec = args.shift(3)
-    @filter = Selector.new(@filter)
+    case @filter
+    when String
+      @filter = Selector.new(@filter)
+    when Lambda
+      # nop
+    else
+      env[:err].puts 'find: second arg must be string or lambda'
+      return false
+    end
     @exec ||= Echo.new
-    Hal['**'].select {|e| @filter.call(e, env:env, frames:frames) }.each {|e| @exec.call(e, env:env, frames:frames) }
+    begin
+      Hal['**'].select {|e| @filter.call(e, env:env, frames:frames) }.each {|e| @exec.call(e, env:env, frames:frames) }
+    rescue VirtualMachine::BreakCalled => err
+      return true
+    end
     true
   end
 end
