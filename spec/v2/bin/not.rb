7f4f45ac19d4b283bc9e16a9c400a0f5c4ec2bfd
edhowland <ed.howland@gmail.com>
2016-08-04 17:59:08 +0000

    \'refactored not to be opposit of exec\'

--- spec/v2/bin/not.rb
+++ spec/v2/bin/not.rb
@@ -1,13 +1,8 @@
 # not - class Not - command not expression - inverts truth of expression
 
-class Not
+class Not < Exec
   def call *args, env:, frames:
-    block = args.shift
-    if block.nil? || !block.instance_of?(Block)
-      env[:err].puts 'not: first arg must be a block'
-      return false
-    end
-    !block.call env:env, frames:frames
+    !super
   end
 end
 
