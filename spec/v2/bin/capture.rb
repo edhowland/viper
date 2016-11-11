158c932bc38147eb9cf88a07774e4b3614a5a5db
edhowland <ed.howland@gmail.com>
2016-11-11 15:46:00 -0500

    \'moved most v2/ .rb to either ast/ or runtime/. purpose to better organize befor implement Buffer here\'

--- spec/v2/bin/capture.rb
+++ spec/v2/bin/capture.rb
@@ -1,6 +1,6 @@
 # capture - class Capture - command capture { block } - runs block, captures any
 # exceptions
-
+require_relative 'exec'
 class Capture < Exec
   def call *args, env:, frames:
     begin
