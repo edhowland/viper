3a1a6f5e7cfb6790bb4cabc49142aa023b015a15
edhowland <ed.howland@gmail.com>
2016-11-15 12:30:19 -0500

    \'added :version variable to vepl, added Vish::DESCRIPTION to -v option to vepl\'

--- spec/v2/version.rb
+++ spec/v2/version.rb
@@ -1,6 +1,14 @@
 # version - class Vish::VERSION - version of Vish shell
 
 class Vish
-  VERSION = '0.1.0'.freeze
+  VERSION = '0.2.0'.freeze
+  DESCRIPTION = <<-EOD
+  Vish is an Bash-like command shell developed for the Viper editor.
+  Thi version #{VERSION} contains support for the Buffer class API and
+  the implementation of the Viper written entirely in various Vish scripts:
+  etc/vishrc - Vish startup script: mounts /v as root of all data structures
+  editor.vsh - Supporting functions for editors
+  viper.vsh - The key bindings for editor mode: viper
+  EOD
 end
 
