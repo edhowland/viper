73c29876b3b5482a9f3f6fcde027848d90c93b59
edhowland <ed.howland@gmail.com>
2016-11-18 20:47:53 -0500

    \'added Tab.advance buffer, called from command tab_goto in bufnode/tab_goto.rb\'

--- spec/v2/api/tabber.rb
+++ spec/v2/api/tabber.rb
@@ -12,12 +12,19 @@ class Tab
     offset = (buffer.at.respond_to?(:tabbed?) ? 1 : 0)
       buffer.to_a[(buffer.position + offset)..-1].index {|e| e.respond_to?(:tabbed?)  } + offset
     end
+
     def next_abs buffer
       buffer.position + next_amt(buffer)
     end
 
     def set buffer
             buffer.apply_at(buffer.position - 1) {|e| e.extend Tabber}
+    end
+    # advances buffer to next tab pt + 1. extra char so pointed at char to right
+    # of current cursor position after advance
+    def advance buffer
+      buffer.goto_position(next_abs(buffer) + 1)
+      ''
     end
   end
 end
