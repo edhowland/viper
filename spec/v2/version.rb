51cefd9d9200630a95d91cbdfa089e49a99768c7
edhowland <ed.howland@gmail.com>
2016-11-26 18:52:13 -0500

    \'updated version.rb to version 2.2\'

--- spec/v2/version.rb
+++ spec/v2/version.rb
@@ -1,14 +1,22 @@
 # version - class Vish::VERSION - version of Vish shell
 
 class Vish
-  VERSION = '0.2.0'.freeze
+  VERSION = '0.2.2'.freeze
   DESCRIPTION = <<-EOD
   Vish is an Bash-like command shell developed for the Viper editor.
   Thi version #{VERSION} contains support for the Buffer class API and
   the implementation of the Viper written entirely in various Vish scripts:
   etc/vishrc - Vish startup script: mounts /v as root of all data structures
-  editor.vsh - Supporting functions for editors
-  viper.vsh - The key bindings for editor mode: viper
+  scripts/editor.vsh - Supporting functions for editors
+  scripts/eviper.vsh - The key bindings for editor mode: viper
+  scripts/edelete.vsh - deletion commands, bound to meta_d + keys
+  scripts/eprompt.vsh - prompting for a choice from user
+  scripts/ecommand.vsh - command mode invoked with meta_semicolon
+  meta.vsh - function meta - loop about meta mode.
+  startup.vsh - gets the whole ball rolling. drops into meta mode with either
+    vip or com mode
+  shutdown.vsh - cleans up unsaved buffers asking to save them.
+  debug.vsh - various aliases for debugging
   EOD
 end
 
