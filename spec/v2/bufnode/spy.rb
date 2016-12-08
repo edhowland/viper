c4aa0109aaa83ef944df7eba7e6e97420941d843
edhowland <ed.howland@gmail.com>
2016-12-08 17:29:39 -0500

    \'removed bufnode/spy.rb, extract.rb - not used with api/buffer.rb\'

--- spec/v2/bufnode/spy.rb
+++
@@ -1,8 +0,0 @@
-# spy - class Spy - command spy - outputs contents of line node.left/right
-
-class Spy < BaseNodeCommand
-  def call *args, env:, frames:
-        perform(args[0], env:env, frames:frames) {|node| node.join('') }
-  end
-end
-
