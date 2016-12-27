92c04e0e6dbde7bfe1830fbde28f29b5c73e2877
edhowland <ed.howland@gmail.com>
2016-12-27 13:20:58 -0500

    \'simple trace functionality added in debugging_support.rb , statements, blocks and functions can be traced\'

--- spec/v2/debugging_support.rb
+++ spec/v2/debugging_support.rb
@@ -1,5 +1,17 @@
 # debugging_support.rb - modules and classes to support debugging
 
+
+$call_stack = []
+
+
+def howdidigethere &blk
+  if block_given?
+    $call_stack.map &blk
+  else
+    $call_stack.map {|e| e.to_s }
+  end
+end
+
 module Debugging
   def call block
     @block_stack = @block_stack || []
@@ -13,3 +25,43 @@ end
 class VirtualMachine
   prepend Debugging
 end
+
+
+module CallRecorder
+  def call env:, frames:
+    $call_stack.push self
+    result = super
+    $call_stack.pop
+  end
+end
+
+class Statement
+  prepend CallRecorder
+end
+
+
+class Block
+  prepend CallRecorder
+ end
+ 
+module CallWArgsRecorder
+  def call *args, env:, frames:
+    $call_stack.push self
+    result = super
+    $call_stack.pop
+  end
+end
+
+
+
+class Function
+  prepend CallWArgsRecorder
+end
+
+class Trace < BaseCommand
+  def call *args, env:, frames:
+    a = howdidigethere {|e| e.class.name + ':' + e.to_s }
+    a.each {|e| env[:out].puts e }
+  end
+end
+  #
