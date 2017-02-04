c63335a976f5e56d0e68183230e6f311af26b044
edhowland <ed.howland@gmail.com>
2017-02-04 13:24:47 -0500

    \'added tests/redirection_test.rb , only happy paths for .new, .call tests so far\'

---
+++ spec/v2/tests/all_tests.rb
@@ -0,0 +1,5 @@
+# all_tests.rb - requires to load all test classes
+
+require_relative 'buffer_test'
+#require_relative 'redirection_test'
+
---
+++ spec/v2/tests/redirection_test.rb
@@ -0,0 +1,41 @@
+# redirection_test.rb - class RedirectionTest -  test  for testing Redirection
+
+require_relative 'test_helper'
+
+class ObjectRedir
+  attr_reader :target, :mode
+end
+
+class RedirectionTest  < BaseSpike
+  def set_up
+    @target = ->(env:, frames:) { 'xxyyzz' }
+    @ios = FrameStack.new
+    @frames = FrameStack.new
+  end
+  def test_init
+    @redir = Redirection.new '>', @target
+  end
+  def test_call
+        @redir = Redirection.new '>', @target
+@redir.call env:@ios, frames:@frames
+assert_is @ios[:out], ObjectRedir
+assert_eq @ios[:out].target, 'xxyyzz'
+assert_eq @ios[:out].mode, 'w'
+  end
+  def test_call_with_stdin
+    @redir = Redirection.new '<', @target
+    result = @redir.call(env:@ios, frames:@frames)
+    assert_nil result
+    assert_is @ios[:in], ObjectRedir
+    assert_eq @ios[:in].target, 'xxyyzz'
+    assert_eq @ios[:in].mode, 'r'
+  end
+  def test_call_with_append
+    @redir = Redirection.new '>>', @target
+    result = @redir.call env:@ios, frames:@frames
+    assert_nil result
+    assert_is @ios[:out], ObjectRedir
+    assert_eq @ios[:out].target, 'xxyyzz'
+    assert_eq @ios[:out].mode, 'a'
+  end
+end
