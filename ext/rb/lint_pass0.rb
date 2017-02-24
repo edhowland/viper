8d36f7728268f6a8753651f268f9c24f419f8b53
edhowland <ed.howland@gmail.com>
2017-02-23 20:28:50 -0500

    \'created partial lint_pass0, checks for non-multiple indentations of :indent, like 1,3, etc\'

---
+++ ext/rb/lint_pass0.rb
@@ -0,0 +1,15 @@
+# lint_pass0.rb - class LintPass0 - checks for indentations not even multiple
+# of :indent
+
+require_relative 'indentations'
+
+class LintPass0 < BaseBufferCommand
+  def call *args, env:, frames:
+    buf_apply args[0], env:env, frames:frames do |buffer|
+      lines = buffer.lines
+      a = 0
+      maker = ->(e) { [a+=1, e] }
+      indentations(lines).map(&maker).reject {|e| (e[1] % frames[:indent].to_i).zero? }.map {|e| "line #{e[0]}: indented: #{e[1]}" }.join("\n")
+    end
+  end
+end
--- ext/rb/lint_pass1.vsh
+++ ext/rb/lint_pass1.vsh
@@ -1,2 +1,5 @@
+require ":{vhome}/ext/rb/lint_pass0.rb"
+install_cmd LintPass0 /v/bin
 require ":{vhome}/ext/rb/lint_pass1.rb"
 install_cmd LintPass1 /v/bin
+
