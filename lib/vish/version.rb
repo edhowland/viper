def442029b7649d1b2091eceeed679a119601680
edhowland <ed.howland@gmail.com>
2017-03-15 10:41:04 -0400

    \'linted lib/vish/version.rb\'

--- lib/vish/version.rb
+++ lib/vish/version.rb
@@ -7,7 +7,8 @@ Viper version #{Vish::VERSION}
 Runs scripts in the Vish command language  implement a code editor.
 The editor scripts are in the Viper Home/scripts directory. Each is a numbered
 file (E.g.: 001_editor.vsh, 002_*.vsh).
-Use -h to see a list of options. By default, viper will open a buffer called : unnamed1
+Use -h to see a list of options. By default, viper will open a
+buffer called : unnamed1
 Any non option arguments are treated as files to be edited.
 Use the --mode mode option to start another mode besides : vip.
 -m com  : starts the command REPL.
@@ -18,4 +19,3 @@ Use the --source to load your own list of Vish scripts or use --execute
 to load a script on the command line.
   EOD
 end
-
