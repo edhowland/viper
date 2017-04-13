970125e376adc1a68e82f12d5fcdbd07abd65eec
edhowland <ed.howland@gmail.com>
2017-04-13 14:13:42 -0400

    \'completed rubocop of lib/bin/capture.rb\'

--- lib/bin/capture.rb
+++ lib/bin/capture.rb
@@ -1,28 +1,31 @@
 # capture.rb - class Capture - command capture
-
-# standalone version
-# temp cpt version - rename to class Capture
+# rubocop:disable Style/Semicolon
 
 class Capture < BaseCommand
   def initialize
     super
     @defaults = [
-      ->(env:, frames:) { wrong_type_num(env:env); false },
-      ->(env:, frames:) { false },
-      ->(env:, frames:) { true }
+      ->(env:, **_keys) { wrong_type_num(env: env); false },
+      ->(**_keys) { false },
+      ->(**_keys) { true }
     ]
   end
 
-  def wrong_type_num env:
-    error "Wrong type or number of arguments. Expected 1 to 3 1 blocks", env:env
+  def trial(args)
+    @defaults.zip(args)
+             .map { |e| e.select { |f| f.respond_to?(:call) } }
+             .map { |e| e[-1] }
   end
 
-  def call *args, env:, frames:
-    trial = @defaults.zip(args).map {|e| e.select {|f| f.respond_to?(:call) } }.map {|e| e[-1] }
-    prosecute, sentence, justice = trial
+  def wrong_type_num(env:)
+    error 'Wrong type or number of arguments. Expects 1 to 3 1 blocks', env: env
+  end
+
+  def call(*args, env:, frames:)
+    prosecute, sentence, justice = trial args
     begin
       env.push
-      result = prosecute.call(env:env, frames:frames)
+      result = prosecute.call(env: env, frames: frames)
     rescue VirtualMachine::ExitCalled => err
       env.pop
       raise err
@@ -31,10 +34,10 @@ class Capture < BaseCommand
       env.pop
       frames.first[:last_exception] = err.message
       result = false
-      sentence.call(env:env, frames:frames)
+      sentence.call(env: env, frames: frames)
     ensure
       env.pop unless env.length <= 1
-      justice.call(env:env, frames:frames)
+      justice.call(env: env, frames: frames)
     end
 
     result
