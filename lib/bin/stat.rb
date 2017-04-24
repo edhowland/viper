4f0d9b194b41543c77913437af0983a60f5a8675
edhowland <ed.howland@gmail.com>
2017-04-24 17:45:35 -0400

    \'after refactor of lib/bin/stat.rb to fully use FlaggedCommand style, before rework into options parsing of -s flag to only output the :to_s of object\'

--- lib/bin/stat.rb
+++ lib/bin/stat.rb
@@ -1,20 +1,23 @@
 # stat - class Stat - command stat file - info about path
 
 class Stat < FlaggedCommand
-  def examine(rxr, method, *args)
-    method.to_s + ' ' + rxr.send(method, *args).to_s
-  end
-
-  def call(*args, env:, frames:)
+  def initialize
+    super(flags: {'-s' => false }) do |inp, out, err, frames, flags, *args|
+      env = {in: inp, out: out, err: err}
     path = args[0]
     info path, *([:virtual?, :directory?]
       .map { |e| examine(Hal, e, path) }), env: env, sep: "\n"
     if Hal.virtual? path
       root = frames[:vroot]
       node = root[path]
-      env[:out].puts "#{node.class.name}: #{node}"
-      env[:out].puts "length: #{node.length}" if node.instance_of?(Array)
+      out.puts "#{node.class.name}: #{node}"
+      out.puts "length: #{node.length}" if node.instance_of?(Array)
     end
     true
+    end
+  end
+
+  def examine(rxr, method, *args)
+    method.to_s + ' ' + rxr.send(method, *args).to_s
   end
 end
