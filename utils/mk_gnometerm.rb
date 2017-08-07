5a72627af00ffbd75adde2bd3a8b6c2cac100316
edhowland <ed.howland@gmail.com>
2017-08-07 11:52:17 -0400

    \'added etc/keymaps/gnometerm.json and utils/mk_gnometerm.rb\'

---
+++ utils/mk_gnometerm.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# mk_gnometerm.rb - creates file ./etc/keymaps/gnometerm.json
+# usage: ./mk_gnometerm.rb > etc/keymaps/gnometerm.json
+
+# keycodes generated in Gnome Terminal emulator in Linux w/Gnome window manager
+# are the Escape (27) followed by the ascii of the character pressed with the Alt key.
+# They get translated to meta_a, meta_b, meta_c ... meta_z
+require 'json'
+
+
+puts( ('a'..'z').to_a.map { |e| [[27, e.ord], "meta_#{e}"] }.to_h.to_json)  
