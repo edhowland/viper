f76435a011b962bdae0d5db08d1f9f2e71575c0f
edhowland <ed.howland@gmail.com>
2018-02-22 14:01:34 -0500

    \'chckpt on pt.rb . Delete works for single stuff 1 item only\'

--- pt.rb
+++ pt.rb
@@ -9,21 +9,41 @@ PieceDescript = Struct.new(:buff, :offset, :length)
 class PieceTable
   def initialize buffer
     @orig = buffer
-    @append = []
+    @append = ""
     @table = [PieceDescript.new(ORIG, 0, @orig.length)]
   end
   attr_reader :orig, :table
   attr_accessor :append
 
-  def to_s
-    @orig.to_s
+  def get_slice(buff, descript)
+    buff.slice(descript.offset, descript.length)
   end
 
+  def to_s
+    @table.reduce("") do |i, j|
+#    binding.pry
+      if j.buff == ORIG
+        i << get_slice(@orig, j)
+      else
+        i << get_slice(@append, j)
+      end
+    end
+  end
+
+  # ante - the part of buff before the descript
+  def ante(descript, offset:)
+    PieceDescript.new(descript.buff, 0, offset)
+  end
+
+  # post - teh bit after the deletion or insert
+  def post(descript, offset:, length:)
+#  binding.pry
+
+    PieceDescript.new(descript.buff, offset+length, descript.length - (offset + length))
+  end
   def delete(offset, length)
     piece = @table.shift
-    before =  PieceDescript.new(piece.buff, piece.offset, (offset - piece.offset))
-    after = PieceDescript.new(piece.buff, (piece.offset + length), (piece.length - length))
-    @table = [before, after]
+    @table = [ante(piece, offset: offset), post(piece, offset: offset, length: length)]
   end
 end
 
