3074cb7452abb6926836331028ff77cd55bf5052
edhowland <ed.howland@gmail.com>
2018-03-09 10:40:52 -0500

    \'removed old pre-functional approach to doing split_at with unneeded functions\'

--- slice_table.rb
+++ slice_table.rb
@@ -15,11 +15,7 @@ class SliceTable
     result
   end
 
-  def within offset, length=-1
-    span = ranges
-    des = span.find {|e| e.include? offset }
-    span.index des
-  end
+
   def offset_of(index)
     ranges.index{|e| e.overlap?(Span.new(index..index)) } 
   end
@@ -52,65 +48,8 @@ class SliceTable
     @table = applyp(gap).flatten
   end
 
-  # returns array of tuples of  logical Spans along with offset within @table
-  def ranges_w_offsets
-    ranges.map {|e| Span.new(e) }.zip((0..@table.length-1).to_a)
-  end
 
-  # Given the filtered logical Spans of those spans which overlap the gap
-  # return the 2 outer tuples of logical Span and offset. First and last
-  def overlapped_regions(gap)
-    regions = ranges_w_offsets.select { |e| e[0].overlap? gap }
-    return regions.first, regions.last
-  end
 
-  # returns the sliced edges off the logical Span edge from the overlapped regions
-  def logical_edges(gap)
-    l_reg, r_reg = overlapped_regions(gap)
-    l_gap = l_reg[0]
-    r_gap = r_reg[0]
-    l_sp = l_gap.overlap(gap)
-    r_sp = gap.overlap(r_gap)
-    return l_sp, r_sp
-
-  end
-
-  def indexes_of_gap(gap)
-    l_reg, r_reg = overlapped_regions(gap)
-    return l_reg[1], r_reg[1]
-  end
-  def actual_slices(gap)
-  l_ndx, r_ndx = indexes_of_gap(gap)
-    return @table[l_ndx], @table[r_ndx]
-  end
-
-  # Return 2 new Slices computed fromlogical  span edges 
-  def actual_adjusted(gap)
-  l_ndx, r_ndx = indexes_of_gap(gap)
-    l_edge, r_edge = logical_edges(gap)
-    l_actual, r_actual = actual_slices(gap)
-
-    l_sp = l_actual.span.from_right(l_edge.length)
-    r_sp = r_actual.span.from_left(r_edge.length)
-     [l_actual.with_span(l_sp), l_ndx, r_actual.with_span(r_sp), r_ndx]
-  end
-  def split_2(gap)
-    l, lndx, r, rndx = actual_adjusted(gap) 
-    @table = @table.select {|e| @table.index(e) < lndx } + [l] + [r] + @table.select { |e| @table.index(e) > rndx }
-  end
-
-  # split_at span
-  def _split_at(span)
-    ndx = within(span.first)
-    r_ndx = within(span.last)
-    if ndx == r_ndx
-      piece = @table[ndx]
-      @table[ndx] = piece.split(span)
-    else
-      split_2(span)
-    end
-    @table.flatten!
-  end
 
   # cleave_at offset of piece_table and offset of slice therin
   def cleave_at(p_off, s_off)
