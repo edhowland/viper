d5169d6af5ae8baa66c62832f71f0e581a7d2368
edhowland <ed.howland@gmail.com>
2018-03-22 15:48:47 -0400

    \'chckpt: added vim.vsc. works for some operations. expect the dereferenceing of :buf object does not always return the correct lambda\'

--- vimc.sh
+++ vimc.sh
@@ -1,4 +1,4 @@
 # vimc - compile vish scrimpts into vim.vsc bytecode
-vishc -o vim.vsc  openf.vs  clip.vs  buffer.vs  marks.vs  startup_c.vs  loop.vs init.vs
+vishc -o vim.vsc  openf.vs  registers.vs  buffer.vs  marks.vs  startup_c.vs  loop.vs init.vs
 
 
