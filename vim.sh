60f218cf1fa6d5f9d4a00e04bf233c0b5aa109a9
edhowland <ed.howland@gmail.com>
2018-03-24 18:01:31 -0400

    \'changed vim.sh to run a0.vs + cmd.vs\'

--- vim.sh
+++ vim.sh
@@ -1,4 +1,4 @@
 # vim.sh - load stuff into Vish scripts in ivs REPL
-ivs -r ./env -r ./buffer_connector -l openf.vs -l registers.vs -l buffer.vs -l marks.vs -l startup.vs -l loop.vs $1
+ivs -r ./env -r ./viper_api -l a0.vs -l cmd.vs
 
 
