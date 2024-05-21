42565716297cb143a49512b780f3edf8c43cd706
Ed Howland <ed.howland@gmail.com>
2024-05-21 11:38:59 -0400

    experiment: about to correct TODO in minitest/lib changing from assert_eq to assert_equal in tests

--- minitest/test_api.rb
+++ minitest/test_api.rb
@@ -8,16 +8,16 @@ class APITests < MiniTest::Test
     path_from_start_elements ''
   end
   def test_path_from_with_empty_full_path_returns_absolute
-    assert_eq '/v/bin', path_from_start_elements('', ['v', 'bin'])
+    assert_equal '/v/bin', path_from_start_elements('', ['v', 'bin'])
   end
   def test_path_from_non_empty_returns_relative_path
-    assert_eq 'bin', path_from_start_elements('v', ['bin'])
+    assert_equal 'bin', path_from_start_elements('v', ['bin'])
   end
   def test_empty_start_and_elements_returns_slash
-    assert_eq '/', path_from_start_elements('')
+    assert_equal '/', path_from_start_elements('')
   end
   def test_relative_dot_dot_returns_dot_dot
-    assert_eq '..', path_from_start_elements('v', ['..'])
+    assert_equal '..', path_from_start_elements('v', ['..'])
   end
 end
 
@@ -44,7 +44,7 @@ class ArrayExtenderTests < MiniTest::Test
   def test_rangify
     r = rangify '2..4'
     assert_is r, Range
-    assert_eq r, 2..4
+    assert_equal r, 2..4
   end
   def test_rangify_returns_nil_if_no_range_string_given
     result = rangify '12'
@@ -52,34 +52,34 @@ class ArrayExtenderTests < MiniTest::Test
   end
   def test_head_or_all_single_element_returns_that_element
     result = head_or_all [55]
-    assert_eq result, 55
+    assert_equal result, 55
   end
   def test_head_or_all_returns_entire_input_if_more_than_element
     input = [0,1,2,3]
         result = head_or_all input
-        assert_eq result, input
+        assert_equal result, input
   end
   def test_default_path_returns_fqn_imput
     # If default_path(/v/foo/bar', /v/foo/bar') => '/v/foo/bar'
-    assert_eq '/v/foo/bar', default_path('/v/foo/bar', default: '/v/foo/bar')
+    assert_equal '/v/foo/bar', default_path('/v/foo/bar', default: '/v/foo/bar')
   end
   def test_default_path_w_just_name_returns_appended_fqn
     # default_path 'baz', default: '/v/cmdlet/misc' => /v/cmdlet/misc/baz
-    assert_eq '/v/cmdlet/misc/baz', default_path('baz', default: '/v/cmdlet/misc')
+    assert_equal '/v/cmdlet/misc/baz', default_path('baz', default: '/v/cmdlet/misc')
   end
   def test_int_or_error
-    assert_eq 0, int_or_error(0)
+    assert_equal 0, int_or_error(0)
   end
   def test_int_or_error_with_string_is_0
-    assert_eq 255, int_or_error('foo')
+    assert_equal 255, int_or_error('foo')
   end
   def test_int_or_error_returns_0_when_string_is_0_
-    assert_eq 0, int_or_error('0')
+    assert_equal 0, int_or_error('0')
   end
   def test_int_or_error_returns_int_value_of_string_number
-    assert_eq 45, int_or_error('45')
+    assert_equal 45, int_or_error('45')
   end
   def test_int_or_error_returns_0_when_blank_string
-    assert_eq 255, int_or_error('')
+    assert_equal 255, int_or_error('')
   end
 end
